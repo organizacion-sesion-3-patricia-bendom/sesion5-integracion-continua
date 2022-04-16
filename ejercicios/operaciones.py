9b0005409183fe52c76d5ec56ba7cee7b7ddd5e7
PATRICIA TIC <86398826+pbendom@users.noreply.github.com>
2022-04-16 12:18:17 +0200

    Update operaciones.py

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,13 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+# PATRICIA. Comprobar si un número dado es par 
+def par(x):
+    
+    if x % 2 == 0:
+        par = 1
+    else:
+        par = 0
+
+    return par
